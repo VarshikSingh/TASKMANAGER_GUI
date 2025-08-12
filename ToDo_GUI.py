bb915b4b0948089f6df629d88162cc0f02e1ff84
VarshikSingh <varshiksingh161@gmail.com>
2025-08-12 21:43:35 +0530

    Reorganized structure : Sepaarated logic and gui

--- ToDo_GUI.py
+++ ToDo_GUI.py
@@ -2,10 +2,44 @@ import json
 import tkinter as tk
 from tkinter.font import BOLD
 
+class TM():
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, task_name):
+            self.tasks.append({'task' : task_name, 'completed' : False})
+            self.save_tasks()
+    
+    def delete_task(self, task_name):
+        for index,task in enumerate(self.tasks):
+            if task['task'] == task_name:
+                self.tasks.pop(index)
+                self.save_tasks()
+                break 
+
+    def mark_complete(self, task_name):        
+        for task in self.tasks:
+            if task['task'] == task_name:
+                task['completed'] = True 
+        self.save_tasks()
+ 
+    def save_tasks(self):
+        with open('tasks.json', 'w') as f :
+            json.dump(self.tasks, f)
+    
+    def load_tasks(self):
+        try:
+            with open('tasks.json', 'r') as f :
+                self.tasks = json.load(f)
+        except FileNotFoundError:
+            self.tasks = []
+        except json.JSONDecodeError:
+            self.tasks = []
+
+
 class ToDoGUI():
     def __init__(self, root , Task_manager):
         
-
         self.root = root
         self.tm = Task_manager 
         self.drag_index = None
@@ -25,19 +59,19 @@ class ToDoGUI():
         self.add_label.grid(row = 1, column = 0)       
         self.entry = tk.Entry(root,width = 30)
         self.entry.grid(row=1, column=1)
-        self.entry.bind("<Return>", lambda event: self.add_task())
+        self.entry.bind("<Return>", lambda event: self.add())
 
         self.del_label = tk.Label(root, text = 'DELETE TASK : ')
         self.del_label.grid(row = 2, column = 0) 
         self.entry2 = tk.Entry(root, width =30)
         self.entry2.grid(row = 2, column = 1)      
-        self.entry2.bind("<Return>", lambda event:  self.delete_task())
+        self.entry2.bind("<Return>", lambda event:  self.delete())
         
         self.mark_label = tk.Label(root, text = 'MARK COMPLETE : ')
         self.mark_label.grid(row = 3, column = 0) 
         self.entry3 = tk.Entry(root, width =30)
         self.entry3.grid(row = 3, column = 1)
-        self.entry3.bind("<Return>", lambda event:  self.mark_complete())
+        self.entry3.bind("<Return>", lambda event:  self.mc())
        
         self.tasks_list = tk.Listbox(root, height = 30, width = 100)
         self.tasks_list.grid(row =5, column =5)     
@@ -46,12 +80,13 @@ class ToDoGUI():
         self.tasks_list.bind("<ButtonRelease-1>", self.on_drop)  
         self.tasks_list.bind("<Button-3>", self.on_rclick)
         
-        
-
         self.save_button = tk.Button(self.root, text = 'EXIT AND SAVE', command = self.exit, padx = 100, pady = 30 )
         self.save_button.grid(row=5, column=0)
 
-        self.load_tasks()
+        self.tm.load_tasks()
+        self.refresh_list()
+
+        
 
     def on_click(self, event):
         self.drag_index = self.tasks_list.nearest(event.y)
@@ -69,81 +104,57 @@ class ToDoGUI():
             self.tasks_list.delete(self.drag_index)
             self.tasks_list.insert(new_index, item)
             self.drag_index = new_index  
+
     def on_drop(self, event):
         self.drag_index = None
 
-
-
-    
-    def add_task(self):
-        task_name = self.entry.get().strip()
-        if task_name:
-            self.tm.tasks.append({'task' : task_name, 'completed' : False})
-            self.entry.delete(0, tk.END)
-            self.refresh_list()
-            self.save_tasks()
-    
-    def delete_task(self):
-        task_name = self.entry2.get().strip()
-        for index,task in enumerate(self.tm.tasks):
-            if task['task'] == task_name:
-                self.tm.tasks.pop(index)
-                self.entry2.delete(0, tk.END)
-                self.refresh_list()
-                self.save_tasks()
-                break 
-    
     def UI_del(self, index ):
         self.tm.tasks.pop(index)
         self.refresh_list()
-        self.save_tasks()
+        self.tm.save_tasks()
         
-
+    def add(self):
+        task_name = self.entry.get().strip()
+        self.tm.add_task(task_name)
+        self.entry.delete(0, tk.END)
+        self.refresh_list()
+        self.tm.save_tasks()
     
-    def mark_complete(self):
+    def delete(self):
+        task_name = self.entry2.get().strip()
+        self.tm.delete_task(task_name)
+        self.entry2.delete(0, tk.END)
+        self.refresh_list()
+        self.tm.save_tasks()
+    
+    def mc(self):
         task_name = self.entry3.get().strip()
-        for task in self.tm.tasks:
-            if task['task'] == task_name:
-                task['completed'] = True 
-        self.entry3.delete(0,tk.END)
-        self.save_tasks()
-        self.refresh_list()        
+        self.tm.mark_complete(task_name)
+        self.entry3.delete(0, tk.END)
+        self.refresh_list()
+        self.tm.save_tasks()    
 
     def UI_mc(self, index):
         self.tm.tasks[index]['completed'] = True
         self.refresh_list()
-        self.save_tasks()
+        self.tm.save_tasks()
               
     def refresh_list(self):
         self.tasks_list.delete(0, tk.END)
         for task in self.tm.tasks:
             status = '✅' if task['completed'] else '❌'
             self.tasks_list.insert(tk.END, f"{task['task']} - {status}")
-            
-    
-    def save_tasks(self):
-        with open('tasks.json', 'w') as f :
-            json.dump(self.tm.tasks, f)
-    
-    def load_tasks(self):
-        try:
-            with open('tasks.json', 'r') as f :
-                self.tm.tasks = json.load(f)
-                self.refresh_list()
-        except FileNotFoundError:
-            self.tm.tasks = []
-        except json.JSONDecodeError:
-            self.tm.tasks = []
     
     def exit(self):
-        self.save_tasks()
+        self.tm.save_tasks()
         self.root.destroy()
 
             
 
-class TM():
-    def __init__(self):
-        self.tasks = []
+
+    
+
+    
 
 root = tk.Tk()
 Tm = TM()
